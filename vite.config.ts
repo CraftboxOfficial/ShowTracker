20122cfa695a5ab38a65b78eb177328181627110
Bartosz Dudziński <bartoszdudzinski18@gmail.com>
2022-10-26 23:52:49 +0200

    enabled pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,35 @@ import mkcert from 'vite-plugin-mkcert'
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [ solidPlugin(), mkcert(), ],
+  plugins: [ solidPlugin(), mkcert(),
+  VitePWA({
+    registerType: "autoUpdate",
+    devOptions: {
+      enabled: true
+    },
+    workbox: {
+      globPatterns: [ "**//*.{ js, css, html, ico, png, svg } " ]
+    },
+    manifest: {
+      name: 'ShowTracker',
+      short_name: 'ShowTracker',
+      description: 'Track yours favorite shows and movies',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/assets/pwaIcons/192x192-icon.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/assets/pwaIcons/512x512-icon.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+  ],
   server: {
     port: 3000,
     https: true
@@ -31,14 +59,14 @@ description: 'Track yours favorite shows and movies',
 theme_color: '#ffffff',
 icons: [
 {
-  src: '/assets/pwaIcons/192x192-icon.png',
-  sizes: '192x192',
-  type: 'image/png'
+src: '/assets/pwaIcons/192x192-icon.png',
+sizes: '192x192',
+type: 'image/png'
 },
 {
-  src: '/assets/pwaIcons/512x512-icon.png',
-  sizes: '512x512',
-  type: 'image/png'
+src: '/assets/pwaIcons/512x512-icon.png',
+sizes: '512x512',
+type: 'image/png'
 }
 ]
 }
