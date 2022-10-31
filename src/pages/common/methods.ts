73eb056c34c3601e40d38990717e5e79acfd54a5
Bartosz Dudziński <bartoszdudzinski18@gmail.com>
2022-10-31 19:32:55 +0100

    added big number shortener

--- src/pages/common/methods.ts
+++ src/pages/common/methods.ts
@@ -1,11 +1,30 @@
-export function getTextDate(date: string | undefined) {
+export function getTextDate(date: string | undefined, options: { day?: boolean, month?: boolean, year?: boolean } = { month: true, year: true }) {
 	const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
 
 	if (date) {
 		const dateArr = date.split("-")
 
-		return `${monthNames[ parseInt(dateArr[ 1 ]) - 1 ]} ${dateArr[ 0 ]}`
+		const day = options.day ? dateArr[ 2 ].replace(/[0^]*/, "") : ""
+		const month = (options.month === undefined ? true : options.month) ? " " + monthNames[ parseInt(dateArr[ 1 ]) - 1 ] : ""
+		const year = (options.year === undefined ? true : options.year) ? " " + dateArr[ 0 ] : ""
+
+		return `${day}${month}${year}`
 	}
 
 	return "Unknown"
+}
+
+export function bigNumberShortener(number: number) {
+
+	if (number > (10 ** 6)) {
+		const shortNumber = (number / (10 ** 6)).toFixed(1)
+		return shortNumber + "m"
+	}
+
+	if (number > (10 ** 3)) {
+		const shortNumber = (number / (10 ** 3)).toFixed(1)
+		return shortNumber + "k"
+	}
+
+	return number
 }
