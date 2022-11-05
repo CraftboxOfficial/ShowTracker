2cf2ab8d73e353a70ba6220f80567e435c60f267
Bartosz Dudziński <bartoszdudzinski18@gmail.com>
2022-11-05 22:45:33 +0100

    added clearing based on date and priority added force refetch option

--- src/components/TmdbProvider.tsx
+++ src/components/TmdbProvider.tsx
@@ -11,25 +11,27 @@ interface TmdbContext {
 	tmdbMultiSearch: (data: {
 		priority: number
 		query: TMDBMultiSearchQuery
+		forceFetch?: boolean
 	}) => Promise<TMDBSearchMultiSearch>,
 
 	tmdbGetConfiguration: () => Promise<TMDBConfigurationGetApiConfiguration>,
 
-	tmdbGetImagesData: HttpsCallable<tmdbGetImagesData, unknown>,
-
 	tmdbGetImage: (data: {
 		priority: number,
 		query: tmdbGetImage
+		forceFetch?: boolean
 	}) => Promise<Blob>,
 
 	tmdbGetTvDetails: (data: {
 		priority: number
 		query: TMDBTvGetDetailsQuery
+		forceFetch?: boolean
 	}) => Promise<TMDBTvGetDetails>,
 
 	tmdbGetTvSeasonsDetails: (data: {
 		priority: number
 		query: TMDBTvSeasonsGetDetailsQuery
+		forceFetch?: boolean
 	}) => Promise<TMDBTvSeasonsGetDetails>,
 	// tmdbGetMovieDetails: (data: {
 	// 	priority: number
@@ -84,9 +86,37 @@ interface savedApiConfiguration extends savedData {
 	4 - 6 -> up to 60 days or space, max up to 50 mb
 	7 - 10 -> up to 30 days or space, max up to 50 mb
 	11 - 13 -> up to 15 days or space, max up to 15 mb at closing; up to 50 mb while running
-	14+ -> cleand on closing
+	14+ -> cleaned on closing
 
  */
+/*
+
+	when I need to fetch:
+		-> on app enter currently watched show that's airing; there may have been some changes
+		-> loading new content
+		-> when user refreshes the page; only currently's open page content
+
+	refetching
+		-> based on priority number of days from creation is calculated by f(x) = (1/3.25) * (x - 14)^2 where x is priority, x === <0; 14> , x > 14 is 0 , x < 0 
+
+	clearing
+		-> when not accessed for half of refetching time then it's removed
+	
+	1 >> 60
+	2 >> 52
+	3 >> 44
+	4 >> 37
+	5 >> 31
+	6 >> 25
+	7 >> 20
+	8 >> 15
+	9 >> 11
+	10 >> 8
+	11 >> 5
+	12 >> 3
+	13 >> 1
+	14 >> 0
+*/
 
 const DB_NAMES = {
 	SEARCH_QUERIES: "searchQueries",
@@ -115,7 +145,12 @@ export const TmdbProvider: Component<{ children: JSXElement }> = (props) => {
 
 			const queryKey = JSON.stringify(data.query)
 
-			const retrivedQuery = await searchQueryStore.getItem(queryKey) as savedSearchQuery | null
+			let retrivedQuery = data.forceFetch ? null : await searchQueryStore.getItem(queryKey) as savedSearchQuery | null
+
+			if (priorityHandler(retrivedQuery) == "toBeRefetched") {
+				console.log("refetch")
+				retrivedQuery = null
+			}
 
 			if (retrivedQuery) {
 				searchQueryStore.setItem(queryKey, {
@@ -145,14 +180,22 @@ export const TmdbProvider: Component<{ children: JSXElement }> = (props) => {
 		tmdbGetConfiguration: async () => {
 			const call = httpsCallable<tmdbGetConfigurationData>(functions, "tmdbGetConfiguration")
 
+			// opens or creates new database instance
 			const configurationStore = localforage.createInstance({
 				name: DB_NAMES.CONFIGURATION
 			})
 
 			const configurationKey = "tmdbApiConfiguration"
 
-			const retrivedConfiguration = await configurationStore.getItem(configurationKey) as savedApiConfiguration | null
+			// if data not found locally returns null
+			let retrivedConfiguration = await configurationStore.getItem(configurationKey) as savedApiConfiguration | null
 
+			if (priorityHandler(retrivedConfiguration) == "toBeRefetched") {
+				console.log("refetch")
+				retrivedConfiguration = null
+			}
+
+			// updates date of last locall access if data found
 			if (retrivedConfiguration) {
 				configurationStore.setItem(configurationKey, {
 					...retrivedConfiguration,
@@ -163,9 +206,10 @@ export const TmdbProvider: Component<{ children: JSXElement }> = (props) => {
 				return retrivedConfiguration.data
 			}
 
-			// if (!retrivedConfiguration) {
+			// fetches data if not found locally
 			const response = await call({ getApiConfiguration: true }) as HttpsCallableResult<tmdbGetConfigurationResponses>
 
+			// saves data locally
 			configurationStore.setItem(configurationKey, {
 				lastUsedOn: new Date(),
 				createdOn: new Date(),
@@ -174,40 +218,36 @@ export const TmdbProvider: Component<{ children: JSXElement }> = (props) => {
 			} as savedApiConfiguration)
 
 			return response.data.apiConfiguration
-			// }
-
 		},
 
-		tmdbGetImagesData: httpsCallable<tmdbGetImagesData>(functions, "tmdbGetImagesData"),
-
-		tmdbGetImage: async (image) => {
+		tmdbGetImage: async (data) => {
 
 			const imageStore = localforage.createInstance({
 				name: DB_NAMES.IMAGES
 			})
 
-			const imageKey = `${image.query.size}${image.query.path}`
+			const imageKey = `${data.query.size}${data.query.path}`
 
-			const retrivedImage = await imageStore.getItem(imageKey) as savedImage | null
+			const retrivedImage = data.forceFetch ? null : await imageStore.getItem(imageKey) as savedImage | null
 
 			if (retrivedImage) {
 				imageStore.setItem(imageKey, {
 					...retrivedImage,
 					lastUsedOn: new Date(),
-					priority: image.priority
+					priority: data.priority
 				} as savedImage)
 
 				return retrivedImage.blob
 			}
 
 			// if (!retrivedImage) {
-			const url = `${image.query.baseUrl}${image.query.size}${image.query.path}`
+			const url = `${data.query.baseUrl}${data.query.size}${data.query.path}`
 			const blob = await (await fetch(url, { method: "GET" })).blob()
 
 			imageStore.setItem(imageKey, {
 				lastUsedOn: new Date(),
 				createdOn: new Date(),
-				priority: image.priority,
+				priority: data.priority,
 				blob: blob
 			} as savedImage)
 
@@ -222,41 +262,37 @@ export const TmdbProvider: Component<{ children: JSXElement }> = (props) => {
 				name: DB_NAMES.DETAILS
 			})
 
-			async function getDetail(data: {
-				priority: number
-				query: TMDBTvGetDetailsQuery
-			}) {
+			const detailsKey = JSON.stringify(data.query)
 
+			let retrivedDetails = data.forceFetch ? null : await detailsStore.getItem(detailsKey) as savedTvDetails | null
 
-				const detailsKey = JSON.stringify(data.query)
-
-				const retrivedDetails = await detailsStore.getItem(detailsKey) as savedTvDetails | null
-
-				if (retrivedDetails) {
-					detailsStore.setItem(detailsKey, {
-						...retrivedDetails,
-						lastUsedOn: new Date(),
-						priority: data.priority
-					} as savedTvDetails)
-
-					return retrivedDetails.data
-				}
-
-				// if (!retrivedDetails) {
-				const response = await call({ ...data.query }) as HttpsCallableResult<TMDBTvGetDetails>
-
-				detailsStore.setItem(detailsKey, {
-					lastUsedOn: new Date(),
-					createdOn: new Date(),
-					priority: data.priority,
-					data: response.data
-				} as savedTvDetails)
-
-				return response.data
-				// }
+			if (priorityHandler(retrivedDetails) == "toBeRefetched") {
+				console.log("refetch")
+				retrivedDetails = null
 			}
 
-			return await getDetail(data)
+			if (retrivedDetails) {
+				detailsStore.setItem(detailsKey, {
+					...retrivedDetails,
+					lastUsedOn: new Date(),
+					priority: data.priority
+				} as savedTvDetails)
+
+				return retrivedDetails.data
+			}
+
+			// if (!retrivedDetails) {
+			const response = await call({ ...data.query }) as HttpsCallableResult<TMDBTvGetDetails>
+
+			detailsStore.setItem(detailsKey, {
+				lastUsedOn: new Date(),
+				createdOn: new Date(),
+				priority: data.priority,
+				data: response.data
+			} as savedTvDetails)
+
+			return response.data
+
 		},
 
 		tmdbGetTvSeasonsDetails: async (data) => {
@@ -266,42 +302,70 @@ export const TmdbProvider: Component<{ children: JSXElement }> = (props) => {
 				name: DB_NAMES.DETAILS
 			})
 
-			async function getDetail(data: {
-				priority: number
-				query: TMDBTvSeasonsGetDetailsQuery
-			}) {
+			const detailsKey = JSON.stringify(data.query)
 
+			let retrivedDetails = data.forceFetch ? null : await detailsStore.getItem(detailsKey) as savedTvSeasonsDetails | null
 
-				const detailsKey = JSON.stringify(data.query)
-
-				const retrivedDetails = await detailsStore.getItem(detailsKey) as savedTvSeasonsDetails | null
-
-				if (retrivedDetails) {
-					detailsStore.setItem(detailsKey, {
-						...retrivedDetails,
-						lastUsedOn: new Date(),
-						priority: data.priority
-					} as savedTvSeasonsDetails)
-
-					return retrivedDetails.data
-				}
-
-				// if (!retrivedDetails) {
-				const response = await call({ ...data.query }) as HttpsCallableResult<TMDBTvSeasonsGetDetails>
-
-				detailsStore.setItem(detailsKey, {
-					lastUsedOn: new Date(),
-					createdOn: new Date(),
-					priority: data.priority,
-					data: response.data
-				} as savedTvSeasonsDetails)
-
-				return response.data
-				// }
+			if (priorityHandler(retrivedDetails) == "toBeRefetched") {
+				console.log("refetch")
+				retrivedDetails = null
 			}
 
-			return await getDetail(data)
+			if (retrivedDetails) {
+				detailsStore.setItem(detailsKey, {
+					...retrivedDetails,
+					lastUsedOn: new Date(),
+					priority: data.priority
+				} as savedTvSeasonsDetails)
+
+				return retrivedDetails.data
+			}
+
+			// if (!retrivedDetails) {
+			const response = await call({ ...data.query }) as HttpsCallableResult<TMDBTvSeasonsGetDetails>
+
+			detailsStore.setItem(detailsKey, {
+				lastUsedOn: new Date(),
+				createdOn: new Date(),
+				priority: data.priority,
+				data: response.data
+			} as savedTvSeasonsDetails)
+
+			return response.data
 		}
+	}
+
+	// f(x) = (1/2.8) * (x - 14)^2 where x is priority, x === <0; 14> , x > 14 is 0 , x < 0 
+
+	function priorityHandler(data: savedData | null, options?: { dataRefetching?: boolean, dataClearing?: boolean }): "toBeCleared" | "toBeRefetched" | null {
+		if (!data) {
+			return null
+		}
+
+		const daysTillRefetch = data.priority > 14 || data.priority < 0 ? 0 : Math.round(parseFloat(((1 / 3.25) * (data.priority - 15) ** 2).toPrecision(2)))
+
+
+		const daysFromCreation = Math.round(((new Date()).getTime() - data.createdOn.getTime()) / 1000 / 3600 / 24)
+		const daysFromLastUse = Math.round(((new Date()).getTime() - data.lastUsedOn.getTime()) / 1000 / 3600 / 24)
+
+		if ((options?.dataRefetching || true) && !options && daysFromCreation >= daysTillRefetch) {
+			// console.log("toBeRefetched")
+			return "toBeRefetched"
+		}
+
+		if (options?.dataClearing && daysFromLastUse >= (daysTillRefetch / 2)) {
+			return "toBeCleared"
+		}
+
+		// console.log(data.createdOn)
+		// console.log(data.lastUsedOn)
+		// console.log(new Date())
+		// // console.log(Math.round(((new Date()).getTime() - data.createdOn.getTime()) / 1000 / 3600 / 24))
+		// console.log(daysFromCreation)
+		// console.log(daysFromLastUse)
+		// console.log(daysTillRefetch)
+
+		return null
 	}
 
 	function cleanupDB() { // TODO add other cache auto cleaning
@@ -313,80 +377,58 @@ export const TmdbProvider: Component<{ children: JSXElement }> = (props) => {
 			name: DB_NAMES.IMAGES
 		})
 
+		const detailsStore = localforage.createInstance({
+			name: DB_NAMES.DETAILS
+		})
+
+		const configurationStore = localforage.createInstance({
+			name: DB_NAMES.CONFIGURATION
+		})
+
 		searchQueryStore.iterate((v: savedSearchQuery, k) => {
-			// console.log(v)
-			if (v.priority >= 14) {
+			if (priorityHandler(v, { dataClearing: true }) == "toBeCleared") {
+				// console.log("clear search " + k)
 				searchQueryStore.removeItem(k)
 			}
 		})
 
-		let imagesSize = {
-			"14": 0,
-			"13": 0,
-			"10": 0,
-			"6": 0,
-			"3": 0,
-			"1": 0
-		}
 		imageStore.iterate((v: savedImage, k) => {
-			if (v.priority == 1) {
-				imagesSize[ 1 ] += v.blob.size
-			}
-
-			if (v.priority >= 2 && v.priority <= 3) {
-				imagesSize[ 3 ] += v.blob.size
-			}
-
-			if (v.priority >= 4 && v.priority <= 6) {
-				imagesSize[ 6 ] += v.blob.size
-			}
-
-			if (v.priority >= 7 && v.priority <= 10) {
-				imagesSize[ 10 ] += v.blob.size
-			}
-
-			if (v.priority >= 11 && v.priority <= 13) {
-				imagesSize[ 13 ] += v.blob.size
-			}
-
-			if (v.priority >= 14) {
-				imagesSize[ 14 ] += v.blob.size
+			if (priorityHandler(v, { dataClearing: true }) == "toBeCleared") {
+				// console.log("clear img " + k)
+				imageStore.removeItem(k)
 			}
 		})
 
-		imageStore.iterate((v: savedImage, k) => {
-			if (v.priority >= 14) {
-				searchQueryStore.removeItem(k)
+		detailsStore.iterate((v: savedData, k) => {
+			if (priorityHandler(v, { dataClearing: true }) == "toBeCleared") {
+				// console.log("clear det " + k)
+				detailsStore.removeItem(k)
 			}
+		})
 
-			if (v.priority <= 13 && v.priority >= 11) {
-				{
-					const currentDate = new Date()
-					const difference = currentDate.getTime() - v.createdOn.getTime()
-					const differenceDays = difference / (1000 * 3600 * 24)
-
-					if (differenceDays >= 15) {
-						searchQueryStore.removeItem(k)
-					}
-				}
-				{
-					// TODO make it prioritize oldest
-					if (imagesSize[ 13 ] > 15 * MEGABYTE) {
-						imagesSize[ 13 ] -= v.blob.size
-						searchQueryStore.removeItem(k)
-					}
-				}
+		configurationStore.iterate((v: savedApiConfiguration, k) => {
+			if (priorityHandler(v, { dataClearing: true }) == "toBeCleared") {
+				// console.log("clear conf " + k)
+				configurationStore.removeItem(k)
 			}
 		})
 	}
+
+	onMount(() => {
+		cleanupDB()
+	})
 
 	onCleanup(() => {
 		cleanupDB()
 	})
 
-	window.addEventListener("beforeunload", (e) => {
-		cleanupDB()
-	})
+	// window.addEventListener("load", (e) => {
+	// 	cleanupDB()
+	// })
+
+	// window.addEventListener("beforeunload", (e) => {
+	// 	cleanupDB()
+	// })
 
 	onMount(() => {
 		cloudFunctions.tmdbGetConfiguration()
